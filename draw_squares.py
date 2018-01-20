0c07e0d994ea4c5f4e76a89eb18b9c22895a8b8f
daiCorrea <daiane.ocorrea@gmail.com>
2018-01-20 11:39:13 -0200

    Walking Jobervaldo

--- draw_squares.py
+++ draw_squares.py
@@ -3,9 +3,12 @@ import turtle
 def draw_square():
     #Creating a new turtle called Jobervaldo. I already love him <3
     jobervaldo = turtle.Turtle()
-    position = jobervaldo.position()
     #Print has to be separated because it's not possible to concatenate the string with a Vec2D object
     print("Jobervaldo started walking in the position: ")
-    print(position)
+    print(jobervaldo.position())
+    #Starting Walking in pixels
+    jobervaldo.forward(100)
+    print("Jobervaldo is now in the position: ")
+    print(jobervaldo.position())
 
 draw_square()
