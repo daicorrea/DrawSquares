9e8d5e8b07b3eefe4a411633758d6635265a4e94
daiCorrea <daiane.ocorrea@gmail.com>
2018-01-20 12:06:59 -0200

    Playing With Functions

--- draw_squares.py
+++ draw_squares.py
@@ -6,26 +6,29 @@ def draw_square():
     jobervaldo.shape("turtle")
     jobervaldo.color("#FFCE60")
     jobervaldo.speed(1)
-    #Creating a runway for Jobervalto to parede
-    runway = turtle.Screen()
-    runway.bgcolor("#21897E")
 
-    #Print has to be separated because it's not possible to concatenate the string with a Vec2D object
-    print("Jobervaldo started walking in the position: ")
-    print(jobervaldo.position())
     #Creating a Square
     for vertices in range (0,4):
         jobervaldo.forward(100)
         jobervaldo.right(90)
-        print("Jobervaldo is now in the position: ")
-        print(jobervaldo.position())
 
-    #Creating a second turtle
+def draw_circle():
+    # Creating a second turtle
     josercleia = turtle.Turtle()
     josercleia.shape("turtle")
     josercleia.color("#EF767A")
     josercleia.circle(100)
 
+#Calling functions together
+def runway():
+    #Creating a runway
+    runway = turtle.Screen()
+    runway.bgcolor("#21897E")
+
+    draw_square()
+    draw_circle()
+
     runway.exitonclick()
 
-draw_square()
+runway()
+
