9fba4ad11be43fe50aac7e65e5cce7d017ae1dd9
daiCorrea <daiane.ocorrea@gmail.com>
2018-01-20 11:47:13 -0200

    Creating a Square With For Loop

--- draw_squares.py
+++ draw_squares.py
@@ -10,10 +10,12 @@ def draw_square():
     #Print has to be separated because it's not possible to concatenate the string with a Vec2D object
     print("Jobervaldo started walking in the position: ")
     print(jobervaldo.position())
-    #Starting Walking in pixels
-    jobervaldo.forward(100)
-    print("Jobervaldo is now in the position: ")
-    print(jobervaldo.position())
+    #Creating a Square
+    for vertices in range (0,4):
+        jobervaldo.forward(100)
+        jobervaldo.right(90)
+        print("Jobervaldo is now in the position: ")
+        print(jobervaldo.position())
 
     runway.exitonclick()
 
