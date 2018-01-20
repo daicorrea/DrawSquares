65f4192afb33d851048946b6ad9999f5d4872d22
daiCorrea <daiane.ocorrea@gmail.com>
2018-01-20 11:28:28 -0200

    Importing Turtle Library to Print Initial Position

--- draw_squares.py
+++ draw_squares.py
@@ -1,4 +1,9 @@
+import turtle
+
 def draw_square():
-    print("draw here")
+    position = turtle.position()
+    #print has to be separated because it's not possible to concatenate the string with a Vec2D object
+    print("Initial position is: ")
+    print(position)
 
 draw_square()
