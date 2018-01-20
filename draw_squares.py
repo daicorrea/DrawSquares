105fb59041ac97c0955af3d821b1517bb1c29795
daiCorrea <daiane.ocorrea@gmail.com>
2018-01-20 11:37:37 -0200

    Creating Jobervaldo: The turtle

--- draw_squares.py
+++ draw_squares.py
@@ -1,9 +1,11 @@
 import turtle
 
 def draw_square():
-    position = turtle.position()
-    #print has to be separated because it's not possible to concatenate the string with a Vec2D object
-    print("Initial position is: ")
+    #Creating a new turtle called Jobervaldo. I already love him <3
+    jobervaldo = turtle.Turtle()
+    position = jobervaldo.position()
+    #Print has to be separated because it's not possible to concatenate the string with a Vec2D object
+    print("Jobervaldo started walking in the position: ")
     print(position)
 
 draw_square()
